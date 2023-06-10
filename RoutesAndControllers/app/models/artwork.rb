abb5832211029af9bfe5f859c1f541f440793f7d
JasonStaubach <jbstaubach@gmail.com>
2023-06-10 11:53:19 -0400

    added to artwork share

--- RoutesAndControllers/app/models/artwork.rb
+++ RoutesAndControllers/app/models/artwork.rb
@@ -2,4 +2,9 @@ class CreateArtwork < ApplicationRecord
     validates :title, presence: true
     validates :image_url, :presence: true
     validates :artist_id, :presence: true, uniqueness: true
+    validates :artist_id, uniqueness: {scope: :title, message: "This title is already used by this artist"}
+
+    belongs_to :artist,
+        foreign_key: :artist_id,
+        class_name: :User
 end
--- RoutesAndControllers/db/migrate/20230608153520_create_create_artwork_shares.rb
+++
@@ -1,8 +0,0 @@
-class CreateCreateArtworkShares < ActiveRecord::Migration[7.0]
-  def change
-    create_table :create_artwork_shares do |t|
-
-      t.timestamps
-    end
-  end
-end

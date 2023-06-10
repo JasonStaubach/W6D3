b3196de9817c9fdb576a16d89135479d6a659f7a
JasonStaubach <jbstaubach@gmail.com>
2023-06-10 13:06:54 -0400

    added seed data

--- RoutesAndControllers/app/models/artwork.rb
+++ RoutesAndControllers/app/models/artwork.rb
@@ -1,7 +1,7 @@
 class Artwork < ApplicationRecord
     validates :title, presence: true
-    validates :image_url, :presence: true
-    validates :artist_id, :presence: true, uniqueness: true
+    validates :image_url, presence: true
+    validates :artist_id, presence: true, uniqueness: true
     validates :artist_id, uniqueness: {scope: :title, message: "This title is already used by this artist"}
 
     belongs_to :artist,
